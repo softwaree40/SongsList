0d4d34fff8559f5409a7a091f755356e30067c06
softwaree40 <softwaree40@gmail.com>
2021-01-13 00:49:01 -0500

    connecting component to the redux store

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,10 @@
 import React ,{ Component } from "react"
-
+import { connect } from "react-redux"
 
 class SongList extends Component {
 
    render(){
-
+         
     return (
         <div>
            <h1>Song List</h1>
@@ -16,4 +16,8 @@ class SongList extends Component {
 
 
 }
-export default SongList
+const mapStateToProps =(state)=>{
+    console.log(state.songs)
+  return {songs: state.songs}
+}
+export default connect(mapStateToProps)(SongList)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,11 @@ import {combineReducers } from "redux"
  const songsReducer = ()=>{
 
     return [
-        {tile: "Water No Get Enemy",duration: "2:30"},
-        {tile: "One Love",duration: "3:30"},
-        {tile: "Keredim",duration: "7:30"},
-        {tile: "Mo Ri KeKe Kan",duration: "9:30"},
-        {tile: "Send Down The Rain",duration: "10:30"}
+        {tile: "Water No Get Enemy",duration: `${new Date()}`},
+        {tile: "One Love",duration: `${new Date()}`},
+        {tile: "Keredim",duration: `${new Date()}`},
+        {tile: "Mo Ri KeKe Kan",duration: `${new Date()}`},
+        {tile: "Send Down The Rain",duration: `${new Date()}`}
 
 ]
 };
